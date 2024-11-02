f8daf048db03c5b4948f7e6cb871e9cfde2c2687
Brian Delwiche <delwiche@google.com>
2024-11-02 02:29:10 +0000

    Fix more memory-unsafe logging
    In various locations around the stack, log statements use structures that
    may, in exceptional cases, have been freed by preceding calls.  This can
    lead to use after free and potentially to security vulnerabilities.
    
    Use local variables instead, or store the length before the call if no
    local variable is already convenient.
    
    Bug: 375404242
    Bug: 375398779
    Bug: 375397720
    Bug: 375397164
    Bug: 375397370
    Bug: 375396810
    Bug: 375159652
    Bug: 375160214
    Bug: 375159480
    Test: m libbluetooth
    Test: researcher POC
    Flag: EXEMPT trivial logic fix
    Ignore-AOSP-First: security
    Tag: #security
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:43cfd234de9ba9557118b0014513269cc1aeefda)
    Merged-In: I6289907e86786eb2e10a163f7fb5d2557eab00bc
    Change-Id: I6289907e86786eb2e10a163f7fb5d2557eab00bc

--- system/stack/rfcomm/rfc_utils.cc
+++ system/stack/rfcomm/rfc_utils.cc
@@ -425,10 +425,12 @@ void rfc_check_send_cmd(tRFC_MCB* p_mcb, BT_HDR* p_buf) {
   /* handle queue if L2CAP not congested */
   while (!p_mcb->l2cap_congested) {
     BT_HDR* p = (BT_HDR*)fixed_queue_try_dequeue(p_mcb->cmd_q);
+
+    uint16_t len = p->len;
     if (p == NULL) break;
     if (L2CA_DataWrite(p_mcb->lcid, p) != L2CAP_DW_SUCCESS) {
       log::warn("Unable to write L2CAP data peer:{} cid:{} len:{}",
-                p_mcb->bd_addr, p_mcb->lcid, p->len);
+                p_mcb->bd_addr, p_mcb->lcid, len);
     }
   }
 }
