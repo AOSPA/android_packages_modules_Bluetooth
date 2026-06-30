AOSPA/android_packages_modules_Bluetooth

commit a1c5dc5f49a791909a3ba55082bbe0c233d5901d

BluetoothInCallService: Fix '+' display in LE Audio calls
In `toLeCall()`, `Uri.toString()` was causing the dialing prefix
'+' to be encoded as '%2B', resulting in incorrect caller ID
displaying on LE Audio devices.

For example, if the URI was `tel:+12345`, in the notification
sent to the remote it would display as `tel:%2B12345`.

This CL switches `toString() to `getScheme()` combined with
`getSchemeSpecificPart()` to pass the unencoded URI and
changes toLeCall() visibility for more robust testing.

Therefore after this change, `tel:+12345` will display
exactly as `tel:+12345`.

Test: atest BluetoothInCallServiceTest
Test: manual PTS test: GTBS/SR/CP/BV-01-C
Bug: 447308191
Flag: EXEMPT BUGFIX
Change-Id: Ie30da3995e3906cd9f46fbe60b4ad319c34f1fb4

android/app/src/com/android/bluetooth/telephony/BluetoothInCallService.java
@@ -1626,7 +1626,8 @@ public class BluetoothInCallService extends InCallService {
         };
     }
 
-    private BluetoothLeCall toLeCall(BluetoothCall call) {
+    @VisibleForTesting
+    BluetoothLeCall toLeCall(BluetoothCall call) {
         Integer state = getTbsCallState(call);
         boolean isConferenceWithNoChildren = isConferenceWithNoChildren(call);
 
@@ -1687,7 +1688,13 @@ public class BluetoothInCallService extends InCallService {
             addressUri = call.getHandle();
         }
 
-        String uri = addressUri == null ? null : addressUri.toString();
+        String uri;
+        if (addressUri == null) {
+            uri = null;
+        } else {
+            uri = addressUri.getScheme() + ":" + addressUri.getSchemeSpecificPart();
+        }
+
         int callFlags = call.isIncoming() ? 0 : BluetoothLeCall.FLAG_OUTGOING_CALL;
 
         String friendlyName = call.getCallerDisplayName();

android/app/tests/unit/src/com/android/bluetooth/telephony/BluetoothInCallServiceTest.java
@@ -41,6 +41,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothLeCall;
 import android.bluetooth.State;
 import android.content.ComponentName;
 import android.content.Context;
@@ -2065,6 +2066,23 @@ public class BluetoothInCallServiceTest {
         mBluetoothInCallService.mLeCallControlClient.onJoinCalls(requestId, uuids);
         verify(mTbsService).requestResult(anyInt(), eq(requestId), eq(Result.SUCCESS));
         verify(firstCall).conference(any(BluetoothCall.class));
+    }
+
+    @Test
+    public void toLeCallDecodesEncodedPlusSign() {
+        UUID callId = UUID.randomUUID();
+        BluetoothCall activeCall = createActiveCall(callId);
+
+        Uri handle = Uri.parse("tel:%2B12345");
+
+        doReturn(handle).when(activeCall).getHandle();
+        doReturn(Call.STATE_ACTIVE).when(activeCall).getState();
+        doReturn(true).when(activeCall).isIncoming();
+        doReturn(null).when(activeCall).getGatewayInfo();
+        doReturn(null).when(activeCall).getParentId();
+
+        BluetoothLeCall leCall = mBluetoothInCallService.toLeCall(activeCall);
+        assertThat(leCall.getUri()).isEqualTo("tel:+12345");
     }
 
     private static void addCallCapability(BluetoothCall call, int capability) {

commit 7d7e7da51db60d387ca9cd5d1f5af69e20b4de02

BT: Fix NPE crash in toLeCall() when call has no address handle
When addressUri is null in toLeCall(), uri was set to null and
passed to BluetoothLeCall(@NonNull String uri, ...), causing
a NullPointerException when .contains() is called on the null
uri inside the TBS stack during onCallAdded.

This regression was introduced by commit a1c5dc5f49a which changed
URI construction from addressUri.toString() to
getScheme() + ":" + getSchemeSpecificPart(), but kept uri = null
for the null-addressUri case.

During MSIM VoLTE conference call merge, the conference call parent
is transiently added with no address handle, hitting this path and
crashing com.android.bluetooth.

Fix: assign uri = "" instead of null to satisfy the @NonNull
contract of BluetoothLeCall constructor.

CRs-Fixed: 4582719
Change-Id: Ie81c930937369b9a95ba19cbc92c2d9db77485e9

android/app/src/com/android/bluetooth/telephony/BluetoothInCallService.java
@@ -2783,12 +2783,7 @@ public class BluetoothInCallService extends InCallService {
             addressUri = call.getHandle();
         }
 
-        String uri;
-        if (addressUri == null) {
-            uri = null;
-        } else {
-            uri = addressUri.getScheme() + ":" + addressUri.getSchemeSpecificPart();
-        }
+        String uri = (addressUri == null) ? "" : addressUri.toString();
 
         int callFlags = call.isIncoming() ? 0 : BluetoothLeCall.FLAG_OUTGOING_CALL;
 